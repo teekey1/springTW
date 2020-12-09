bd9a1dcb7a6c909e656cbf8587d28bcc9250031e
Tomek K <tomek.krogulec@o2.pl>
2020-12-10 00:17:04 +0100

    added UserService

---
+++ src/main/java/com/codecool/springTW/services/UserService.java
@@ -0,0 +1,32 @@
+package com.codecool.springTW.services;
+
+import com.codecool.springTW.models.User;
+import com.codecool.springTW.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id){
+        return userRepository.findById(id);
+    }
+
+    public void removeUser(Long id){
+        userRepository.deleteById(id);
+    }
+}
