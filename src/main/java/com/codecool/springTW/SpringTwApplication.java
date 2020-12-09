8980fa574e7fe92d1a17a7609c183489440741c7
Tomek K <tomek.krogulec@o2.pl>
2020-12-10 00:14:44 +0100

    Initial commit

---
+++ src/main/java/com/codecool/springTW/SpringTwApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.springTW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecool/springTW/SpringTwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.springTW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
